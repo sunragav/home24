21cdc05bead2dc1fd7a0d42105badafebb786e61
Sundara Raghavan <sunragav@gmail.com>
2019-12-26 21:29:06 +0100

    UI for the Review screen and changes to display the listview and grid view correctly.

--- data/src/main/java/com/sunragav/home24/data/ArticlesRepositoryImpl.kt
+++ data/src/main/java/com/sunragav/home24/data/ArticlesRepositoryImpl.kt
@@ -66,7 +66,7 @@ class ArticlesRepositoryImpl @Inject constructor(
 
         private fun requestAndSaveData() {
 
-            if (isRequestInProgress || query.flagged) return
+            if (isRequestInProgress || query.flagged || query.reviewed) return
             repositoryStateRelay.relay.accept(RepositoryState.LOADING)
             println("Requesting page$lastRequestedPage")
             isRequestInProgress = true
